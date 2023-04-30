94cce92ccbff3b0b4ad46e6204ac7aa9b2bd33ed
Anideep <anideepkalia@gmail.com>
2023-04-30 12:58:19 +0530

    updating task screen

--- src/screens/TaskScreen.js
+++ src/screens/TaskScreen.js
@@ -13,68 +13,73 @@ import {
 const TaskScreen = () => {
   const data = [{
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    fullName: "Aafreen Khan",
-    timeStamp: "12:47 PM",
-    recentText: "Good Day!",
-    avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    title: "Aafreen Khan",
+    locationName: "locationName",
+    timestr: "12:47 PM",
+    description: "Good Day!",
+    avatarUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Eo_circle_blue_letter-d.svg/768px-Eo_circle_blue_letter-d.svg.png?20200417110758"
   }, {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    fullName: "Sujitha Mathur",
-    timeStamp: "11:11 PM",
-    recentText: "Cheer up, there!",
-    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU"
+    title: "Sujitha Mathur",
+    locationName: "Sujitha Mathur",
+    timestr: "11:11 PM",
+    description: "Cheer up, there!",
+    avatarUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Eo_circle_teal_letter-t.svg/768px-Eo_circle_teal_letter-t.svg.png?20200417180559"
   }, {
     id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    fullName: "Anci Barroco",
-    timeStamp: "6:22 PM",
-    recentText: "Good Day!",
+    title: "Anci Barroco",
+    locationName: "Sujitha Mathur",
+    timestr: "6:22 PM",
+    description: "Good Day!",
     avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg"
   }, {
     id: "68694a0f-3da1-431f-bd56-142371e29d72",
-    fullName: "Aniket Kumar",
-    timeStamp: "8:56 PM",
-    recentText: "All the best",
+    title: "Aniket Kumar",
+    locationName: "Sujitha Mathur",
+    timestr: "8:56 PM",
+    description: "All the best",
     avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
   }, {
     id: "28694a0f-3da1-471f-bd96-142456e29d72",
-    fullName: "Kiara",
-    timeStamp: "12:47 PM",
-    recentText: "I will call today.",
+    title: "Kiara",
+    locationName: "Sujitha Mathur",
+    timestr: "12:47 PM",
+    description: "I will call today.",
     avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
   }];
   return (
-    <NativeBaseProvider>
-      
-<Box>
-      <Heading fontSize="xl" p="4" pb="3">
-        Inbox
+    <NativeBaseProvider>     
+<Box mt="35px">
+      <Heading fontSize="4xl" mx="auto" p="4" pb="3">
+        Today's Task
       </Heading>
       <FlatList data={data} renderItem={({
       item
-    }) => <Box borderBottomWidth="1" _dark={{
-      borderColor: "muted.50"
-    }} borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
+}) => <Box borderBottomWidth="2" _light={{
+  borderColor: "muted.50"
+}} borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
             <HStack space={[2, 3]} justifyContent="space-between">
-              <Avatar size="48px" source={{
+              <Avatar size="48px" style={{backgroundColor:'white'}} source={{
           uri: item.avatarUrl
         }} />
               <VStack>
-                <Text _dark={{
-            color: "warmGray.50"
-          }} color="coolGray.800" bold>
-                  {item.fullName}
+              <Text style={{fontWeight: 'bold',fontSize: 20}}>
+                  {item.title}
+                </Text>
+              <Text style={{fontStyle: 'italic',fontSize: 15}}>
+                  {item.locationName}
                 </Text>
                 <Text color="coolGray.600" _dark={{
             color: "warmGray.200"
           }}>
-                  {item.recentText}
+                  {item.description}
                 </Text>
               </VStack>
               <Spacer />
               <Text fontSize="xs" _dark={{
           color: "warmGray.50"
         }} color="coolGray.800" alignSelf="flex-start">
-                {item.timeStamp}
+                {item.timestr}
               </Text>
             </HStack>
           </Box>} keyExtractor={item => item.id} />
